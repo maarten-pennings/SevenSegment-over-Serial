a2f7a3e0ddd4a58f6dedd62a111547efe06f8f3d
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2022-05-08 13:53:22 +0200

    icsp.bat for SSoS firmware

---
+++ firmware/SSoS/icsp.bat
@@ -0,0 +1,3 @@
+REM Uses avrdude and a programmer to flash the hex file into an ATmega328PB
+SET AVRDUDE=%APPDATA%\..\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17
+%AVRDUDE%\bin\avrdude -C%AVRDUDE%\etc\avrdude.conf -v -patmega328pb -cstk500v1 -PCOM5 -b19200 -Uflash:w:SSoS.ino.eightanaloginputs.hex:i
