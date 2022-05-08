4f6449d8a01c850bdda7bf57411a57229ddbcaab
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2022-05-08 13:52:49 +0200

    hex file user independent

--- electronics2/flashing/ledtest/icsp.bat
+++ electronics2/flashing/ledtest/icsp.bat
@@ -1,2 +1,3 @@
-SET AVRDUDE=C:\Users\maarten\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17
+REM Uses avrdude and a programmer to flash the hex file into an ATmega328PB
+SET AVRDUDE=%APPDATA%\..\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17
 %AVRDUDE%\bin\avrdude -C%AVRDUDE%\etc\avrdude.conf -v -patmega328pb -cstk500v1 -PCOM5 -b19200 -Uflash:w:ledtest.ino.eightanaloginputs.hex:i
